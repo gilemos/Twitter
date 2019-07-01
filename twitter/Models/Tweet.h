5c13f0bfba66cb39c551c87258d6211f358987b8
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-01 10:46:23 -0700

    **Created the Tweet Data Model** Created a data model for the tweet that contains several properties and two methods: one to initialize the tweet according to a dictionary and one of create an array of tweets based on an array of dictionaries. No errors are appearing

---
+++ twitter/Models/Tweet.h
@@ -0,0 +1,54 @@
+//
+//  Tweet.h
+//  twitter
+//
+//  Created by gilemos on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tweet : NSObject
+
+
+// ---- GENERAL PROPERTIES -----
+
+//This property is an unique identifier for the tweet in string format
+@property (strong, nonatomic) NSString * idString;
+
+//This property contains the text of the tweet
+@property (strong, nonatomic) NSString * text;
+
+//This property shows the number of favourites that the tweet has
+@property(nonatomic) int favoriteCount;
+
+//This property indicated wheater the tweet is favourited (the little heart is red) or not
+@property(nonatomic) BOOL favorited;
+
+//This property shows the number of times the tweet was retweeted
+@property(nonatomic) int retweetCount;
+
+//This property shows wheater the tweet was retweeted or not
+@property(nonatomic) BOOL retweeted;
+
+//This property indicates who is the user that created the tweet. It includes the name and screen name of the
+//user
+@property(strong, nonatomic) User *user;
+
+//This property is a string with the date when the tweet was published
+@property(strong, nonatomic) NSString *createdAtString;
+
+
+
+// ----- FOR RETWEETS -----
+
+//This property indicates the user who retweeted the tweet
+@property (strong, nonatomic) User *retweetedByUser;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,67 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by gilemos on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+// This method initializes the properties based on the dictionary returned from the API
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+
+        // Is this a re-tweet?
+        NSDictionary *originalTweet = dictionary[@"retweeted_status"];
+        
+        //If it is a retweet, then set the property retweeted by user
+        if(originalTweet != nil){
+            NSDictionary *userDictionary = dictionary[@"user"];
+            self.retweetedByUser = [[User alloc] initWithDictionary:userDictionary];
+            
+            // Change tweet to original tweet
+            dictionary = originalTweet;
+        }
+        self.idString = dictionary[@"id_str"];
+        self.text = dictionary[@"text"];
+        self.favoriteCount = [dictionary[@"favorite_count"] intValue];
+        self.favorited = [dictionary[@"favorited"] boolValue];
+        self.retweetCount = [dictionary[@"retweet_count"] intValue];
+        self.retweeted = [dictionary[@"retweeted"] boolValue];
+        
+        //Initializing the user
+        NSDictionary *user = dictionary[@"user"];
+        self.user = [[User alloc] initWithDictionary:user];
+        
+        //Formatting and setting createdAtString
+           //The original string is in the format “Wed Aug 27 13:08:45 +0000 2008”
+        NSString *createdAtOriginalString = dictionary[@"created_at"];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+           // Configuring the input format to parse the date string
+        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+          // Converting String to Date format
+        NSDate *date = [formatter dateFromString:createdAtOriginalString];
+          // Configuring output format
+        formatter.dateStyle = NSDateFormatterShortStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+          // Convertign Date back to String format
+        self.createdAtString = [formatter stringFromDate:date];
+    }
+    return self;
+}
+
+//This method returns an array of all tweets initialized with an array of tweet dictionaries
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries{
+    NSMutableArray *tweets = [NSMutableArray array];
+    for (NSDictionary *dictionary in dictionaries) {
+        Tweet *tweet = [[Tweet alloc] initWithDictionary:dictionary];
+        [tweets addObject:tweet];
+    }
+    return tweets;
+}
+
+@end
