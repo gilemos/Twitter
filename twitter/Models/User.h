cfd519321f9bd3bc23296f2b8d5eea1d9fc675d0
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-01 09:40:17 -0700

    **Created the User Model Class** Created an user model class that has the properties of name and screen name and an initializer that initiated those two properties based on the dictionary that is returned from the API

---
+++ twitter/Models/User.h
@@ -0,0 +1,27 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by gilemos on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+//This is the name of the user as he or she was registered in the twitter
+@property (strong, nonatomic) NSString *name;
+
+//This is the name of the user that appears for his friends and followers
+@property (strong, nonatomic) NSString *screenName;
+
+
+//This method sets all our properties based on the dictionary that is returned from the API
+- (instancetype)initWithDictionary:(NSDictionary *) dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Models/User.m
@@ -0,0 +1,28 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by gilemos on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+//This method sets all our properties based on the dictionary that is returned from the API
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    //Calling the "pseudo-constructor" of the mother class
+    self = [super init];
+    
+    //If the memory allocation did not fail, proceed
+    if (self) {
+        self.name = dictionary[@"name"];
+        self.screenName = dictionary[@"screen_name"];
+    }
+    
+    return self;
+}
+@end
+
+
