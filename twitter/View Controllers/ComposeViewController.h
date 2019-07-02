c3a53fa0398478313cc7b3da9d3f0d2cc344bdda
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-01 17:53:25 -0700

    **Compose View Controller - Begin** Created a compose view controlled and enabled the close button. However, you cannot tweet something yet

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeViewController.h
+//  
+//
+//  Created by gilemos on 7/1/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,36 @@
+//
+//  ComposeViewController.m
+//  
+//
+//  Created by gilemos on 7/1/19.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)tweetButtom:(id)sender {
+}
+- (IBAction)closeButtom:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
